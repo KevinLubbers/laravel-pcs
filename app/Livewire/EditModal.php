2794a89f9124ce4e35afc10be3ec1aa2f848c480
KevinLubbers <kelubbers@gmail.com>
2026-01-18 23:58:54 -0500

    (fixed) alignment of buttons on specialist screen - working on fixing Edit Model modal - wanting current specialist to populate

--- app/Livewire/EditModal.php
+++ app/Livewire/EditModal.php
@@ -76,7 +76,7 @@ class EditModal extends Component
                 {{session('success')}}
             </div>
         @endif
-        <div x-data="{ show: @entangle('isOpen'), id: '', name: '', specialist: '', title: '' }" x-show="show"
+        <div x-data="{ show: @entangle('isOpen'), id: '', name: '', specialist: @entangle('specialist'), title: '' }" x-show="show"
         @edit-division.window="show = !show, id = $event.detail.id, name = $event.detail.name, specialist = $event.detail.specialist, title = $event.detail.title"
         @edit-model.window="show = !show, id = $event.detail.id, name = $event.detail.name, specialist = $event.detail.specialist, title = $event.detail.title"  > 
             <x-dialog-modal>
@@ -89,12 +89,11 @@ class EditModal extends Component
                         <x-label for="name" value="{{ __('Name') }}" />
                         <x-input x-data={} @edit-division.window="$nextTick(() => $refs.namecheck.focus())"
                         @edit-model.window="$nextTick(() => $refs.namecheck.focus())" x-model="name" x-ref="namecheck" type="text" name="name" id="name" autocomplete="off" />
-                        <x-label for="id" value="{{ __('Assign Specialist') }}" />
-                        <select  x-model="specialist" class="mt-1 block mb-2 rounded-md text-gray-600 border-gray-300   dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 shadow-sm">
-                            <option disabled selected value="0">Select Specialist</option>
-                            <option selected value="0">No Specialist</option>
+                        <x-label for="specialist" value="{{ __('Assign Specialist') }}" />
+                        <select wire:model="specialist"  class="mt-1 block mb-2 rounded-md text-gray-600 border-gray-300   dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 shadow-sm">
+                            <option disabled value="0">Select Specialist</option>
                             @foreach($specialists as $id => $name)
-                                <option id="specialist_id" name="specialist_id" id="{{$id}}" value="{{$id}}" >{{$name}}</option>
+                                <option x-bind:selected="specialist == {{$id}}" id="{{$id}}" value="{{$id}}" >{{$name}}</option>
                             @endforeach
                         </select>
                     </div>
--- resources/views/livewire/specialist-row.blade.php
+++ resources/views/livewire/specialist-row.blade.php
@@ -1,4 +1,4 @@
-<div wire:key="{{$specialist->id}}" class="flex flex-row flex-wrap gap-6 justify-between p-6 items-center" x-data="{unlockClicked: false, deleteClicked: false, id: '{{$specialist->id}}', name: '{{$specialist->name}}', email: '{{$specialist->email}}'}">
+<div wire:key="{{$specialist->id}}" class="flex flex-row flex-wrap gap-6 p-6" x-data="{unlockClicked: false, deleteClicked: false, id: '{{$specialist->id}}', name: '{{$specialist->name}}', email: '{{$specialist->email}}'}">
     @php
         $unlocked = "imgs/unlocked.png";
         $locked = "imgs/locked.png";
@@ -15,11 +15,11 @@
             <x-input x-ref="focus" x-init="$watch('name', value => $dispatch('edited-name', {id: id, name: name}))" x-model.lazy="name"  id="name{{$specialist->id}}"  type="text" class="block w-full" x-bind:disabled="!unlockClicked"  />
             <x-input x-init="$watch('email', value => $dispatch('edited-email', {id: id, email: email}))" x-model.lazy="email" id="email{{$specialist->id}}" type="email" class="block w-full" x-bind:disabled="!unlockClicked"  />
             </div>
-            <div class="flex flex-col items-center">
-                <div class="flex items-center justify-center">
-                    <img height="32px" width="32px" class="mt-4 mb-2 ml-2"  :src="!unlockClicked ? '{{ url($locked) }}' : '{{ url($unlocked) }}'" >
+            <div class="flex flex-col">
+                <div class="flex justify-center">
+                    <img height="32px" width="32px" class="mt-6 mb-2 ml-2"  :src="!unlockClicked ? '{{ url($locked) }}' : '{{ url($unlocked) }}'" >
                 </div>
-                <div class="flex flex-row items-center">
+                <div class="flex flex-row">
                     <img @click="unlockClicked = !unlockClicked; $nextTick(() => $refs.focus.focus());"  height="32px" width="32px" class="mr-1 cursor-pointer" :src="!darkMode ? '{{url($light)}}' : '{{url($dark)}}'" >
                     <img @click="deleteClicked = !deleteClicked" wire:click="deleteSpecialist({{$specialist->id}})" wire:confirm="Are you sure you want to DELETE - {{$specialist->name}}"  height="32px" width="32px" class="mr-1 cursor-pointer" :src="!darkMode ? '{{url($dLight)}}' : '{{url($dDark)}}'" >
                 </div>
